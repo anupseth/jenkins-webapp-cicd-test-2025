f5ff7f44c8adfdde242f1d6849b335794892239d
Anupkumar Seth <anupseth11@yahoo.com>
2025-01-28 21:07:52 +0100

    initial commit

---
+++ src/main/java/com/example/JenkinsWebappCicdTest2025Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebappCicdTest2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebappCicdTest2025Application.class, args);
+	}
+
+}
